14522ca6c9db0dfe7441f13aa55b9ff75579e833
Shrey Patel <129392971+sparse-14@users.noreply.github.com>
2023-10-30 17:07:28 +0530

    Update Person.js

--- movie/src/Person.js
+++ movie/src/Person.js
@@ -127,7 +127,7 @@ function Person() {
   const getInitialData = async () => {
     try {
       const usermail = localStorage.getItem("moviemail");
-      let result = await fetch("http://localhost:5000/initialdata", {
+      let result = await fetch("https://movie-backend-3omk.onrender.com/initialdata", {
         method: "post",
         body: JSON.stringify({ usermail }),
         headers: {
@@ -154,7 +154,7 @@ function Person() {
       console.log(id);
       console.log(media_type);
       const usermail = localStorage.getItem("moviemail");
-      let result = await fetch("http://localhost:5000/watchlist", {
+      let result = await fetch("https://movie-backend-3omk.onrender.com/watchlist", {
         method: "post",
         body: JSON.stringify({ media_type, id, usermail }),
         headers: {
